90baccb6be6676befe5333a2d84316b4b8837947
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-04-07 18:39:25 -0500

    Add placeholder for sector chart

--- app/assets/javascripts/google-maps.js
+++ app/assets/javascripts/google-maps.js
@@ -25,20 +25,4 @@ function initMap() {
       })
     }
   });
-
-  // Construct the circle for each value in citymap.
-  // Note: We scale the area of the circle based on the population.
-  // for (var city in citymap) {
-  //   // Add the circle for this city to the map.
-  //   var cityCircle = new google.maps.Circle({
-  //     strokeColor: '#FF0000',
-  //     strokeOpacity: 0.8,
-  //     strokeWeight: 2,
-  //     fillColor: '#FF0000',
-  //     fillOpacity: 0.35,
-  //     map: map,
-  //     center: citymap[city].center,
-  //     radius: Math.sqrt(citymap[city].population) * 100
-  //   });
-  // }
 }
