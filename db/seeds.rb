83aebd6c43a513dd9bcac053a5073c6cd8f065e4
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-04-09 23:11:13 -0500

    Import countries and lat/lng from https://developers.google.com/public-data/docs/canonical/countries_csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+ActiveRecord::Base.transaction do
+  CSV.foreach(File.dirname(__FILE__) + '/countries.csv', :headers => true) do |row|
+    Country.create!(row.to_hash)
+  end
+end
