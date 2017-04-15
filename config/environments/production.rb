59a2cef3ea5b27f05a07b7319f4b05e2ef7db2fc
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-04-15 19:48:32 +0100

    Fix action mailer domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
     address:        ENV['MAILGUN_SMTP_SERVER'],
     user_name:      ENV['MAILGUN_SMTP_LOGIN'],
     password:       ENV['MAILGUN_SMTP_PASSWORD'],
-    domain:         'msa-registry.heroku.com',
+    domain:         'msa-registry.herokuapp.com',
     authentication: :plain
   }
 
