9c0860dc0dd4b033f0645f20944ff803e9a64f78
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-04-15 20:37:39 +0100

    Require confirmation after signing up

--- features/step_definitions/security_steps.rb
+++ features/step_definitions/security_steps.rb
@@ -2,7 +2,8 @@ Given(/^(Joe|Patricia) is logged in$/) do |actor|
   user = User.create!({
     email: "#{actor.name}@host.com",
     password: 's3cr3t',
-    admin: true
+    admin: true,
+    confirmed_at: DateTime.current
   })
   login_as(user)
 end
