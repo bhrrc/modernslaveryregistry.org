1882c1428617997190aa426b157a683b96154a89
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-05-02 22:48:51 +0100

    Bugfix: Download statements while not logged in

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,10 +1,12 @@
 class ExploreController < ApplicationController
+  include ApplicationHelper
+
   def index
     @statements = Statement.search(current_user, params)
     respond_to do |format|
       format.html
       format.csv do
-        send_data Statement.to_csv(@statements, current_user.admin?),
+        send_data Statement.to_csv(@statements, admin?),
           filename: "modernslaveryregistry-#{Date.today}.csv"
       end
     end
