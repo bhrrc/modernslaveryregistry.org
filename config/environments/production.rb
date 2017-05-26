ec83e67ac48ff5d58b9dcbb933ec6fa15989de5d
Josh Chisholm <joshuachisholm@gmail.com>
2017-05-26 15:00:31 +0100

    Use correct host in mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "msa-registry_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'msa-registry.herokuapp.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'modernslaveryregistry.org', protocol: 'https' }
 
   config.action_mailer.smtp_settings = {
     user_name:      ENV['SENDGRID_USERNAME'],
