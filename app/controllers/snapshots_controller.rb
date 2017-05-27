a58022b6d29a70e95eaecdd9fa792a210fc78d2e
Josh Chisholm <joshuachisholm@gmail.com>
2017-05-27 23:57:05 +0100

    Fix snapshot controller

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -1,6 +1,6 @@
 class SnapshotsController < ApplicationController
   def show
-    @statement = Statement.find(params[:statement_id])
-    send_data @statement.content_data, type: @statement.content_type, disposition: 'inline'
+    snapshot = Statement.find(params[:statement_id]).snapshot
+    send_data snapshot.content_data, type: snapshot.content_type, disposition: 'inline'
   end
 end
