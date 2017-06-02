7d91adcc5ae2e46a498203a82c441ed1761155f3
Josh Chisholm <joshuachisholm@gmail.com>
2017-06-02 17:40:07 +0100

    Lint

--- app/models/screen_grab.rb
+++ app/models/screen_grab.rb
@@ -1,18 +1,8 @@
 class ScreenGrab
   def self.fetch(url)
     kit = IMGKit.new(url)
-    FetchResult.with(
-      url: url,
-      broken_url: false,
-      content_data: kit.to_jpg,
-      content_type: 'image/jpeg'
-    )
+    FetchResult.with(url: url, broken_url: false, content_data: kit.to_jpg, content_type: 'image/jpeg')
   rescue IMGKit::CommandFailedError
-    FetchResult.with(
-      url: url,
-      broken_url: true,
-      content_data: nil,
-      content_type: nil
-    )
+    FetchResult.with(url: url, broken_url: true, content_data: nil, content_type: nil)
   end
 end
