446b56386652e6d0081652671b1eb36188554d5c
Josh Chisholm <joshuachisholm@gmail.com>
2017-06-16 13:25:03 +0100

    D'uh typo

--- app/jobs/fetch_statement_snapshot_job.rb
+++ app/jobs/fetch_statement_snapshot_job.rb
@@ -2,6 +2,6 @@ class FetchStatementSnapshotJob < ApplicationJob
   queue_as :default
 
   def perform(statement_id)
-    Statment.find(statement_id).fetch_statement_from_url!
+    Statement.find(statement_id).fetch_statement_from_url!
   end
 end
