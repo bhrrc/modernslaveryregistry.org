60afe93dd0264da9bad0d34fb11fbd45e2107c42
Josh Chisholm <joshuachisholm@gmail.com>
2018-02-18 12:42:38 +0000

    Bug fix: incorrect case of 'Not explicit' in validator

--- features/step_definitions/legislation_steps.rb
+++ features/step_definitions/legislation_steps.rb
@@ -3,7 +3,8 @@ Given(/^the following legislations exist:$/) do |table|
     Legislation.create!(
       name: hash['Name'],
       icon: hash['Name'].downcase.underscore,
-      include_in_compliance_stats: hash['Include in compliance stats?'] == 'Yes'
+      include_in_compliance_stats: hash['Include in compliance stats?'] == 'Yes',
+      requires_statement_attributes: hash['Requires statement attributes'] || ''
     )
   end
 end
