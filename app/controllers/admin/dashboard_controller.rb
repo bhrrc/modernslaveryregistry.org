f5ec7b4ccc2b0cf7bdd007f37b7bbc246ef1294d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-02-28 01:15:52 +0000

    Sort new drafts to make it easier to spot dupes

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,9 @@
 module Admin
   class DashboardController < AdminController
-    def show
-      @new_statements = Statement.where(published: false).includes(:company).order('created_at DESC')
-      @broken_urls = Statement.where(broken_url: true).includes(:company).order('created_at DESC')
+    def show # rubocop:disable Metrics/AbcSize
+      compare = ->(a, b) { a.company.name <=> b.company.name }
+      @new_statements = Statement.where(published: false).includes(:company).order('created_at DESC').sort(&compare)
+      @broken_urls = Statement.where(broken_url: true).includes(:company).order('created_at DESC').sort(&compare)
       @compliance_stats = ComplianceStats.compile
     end
   end
