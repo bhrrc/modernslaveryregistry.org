e767727c81210ea6e8fecc6ff9dfabbd03efbc94
Chris Roos <chris@seagul.co.uk>
2018-06-19 11:45:53 +0100

    Reduce size of stored snapshot images
    We have some very large image snapshots in the database (the 10 largest
    images are between 18MB and 350MB).
    
    Storing the images as png with a quality of 1 appears to give the
    highest quality for the lowest file size.
    
    This will only affect new snapshots. I'll resize the existing ones
    separately.
    
    There's more information in
    https://trello.com/c/Nbk1otVM/212-very-large-image-screenshots.

--- app/models/screen_grab.rb
+++ app/models/screen_grab.rb
@@ -1,7 +1,7 @@
 class ScreenGrab
   def self.fetch(url)
-    kit = IMGKit.new(url)
-    FetchResult.with(url: url, broken_url: false, content_data: kit.to_jpg, content_type: 'image/jpeg')
+    kit = IMGKit.new(url, quality: 1)
+    FetchResult.with(url: url, broken_url: false, content_data: kit.to_png, content_type: 'image/png')
   rescue IMGKit::CommandFailedError => e
     Rails.logger.error e
     FetchResult.with(url: url, broken_url: true, content_data: nil, content_type: nil)
