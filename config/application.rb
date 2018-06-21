91d2f7616c6b3a027c58e50215f7d5819bee440d
Chris Roos <chris@seagul.co.uk>
2018-06-21 08:01:55 +0100

    Upgrade to Rails 5.2
    And run `rails app:update` task to generate updated application files
    for Rails 5.2.
    
    Note that new Rails 5.2 apps include bootsnap by default[1]. It's not
    obvious that we need it so I've removed the `require 'bootsnap/setup'`
    line that was added to boot.rb.
    
    [1]: http://guides.rubyonrails.org/upgrading_ruby_on_rails.html#bootsnap

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'action_cable/engine'
 require 'sprockets/railtie'
+require 'active_storage/engine'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -22,8 +23,9 @@ module MsaRegistry
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
     config.active_job.queue_adapter = :sidekiq
