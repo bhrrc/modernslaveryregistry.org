00989814305d4edd96529d4f7c5bea8fef5a0712
Chris Roos <chris@seagul.co.uk>
2018-06-21 09:08:36 +0100

    Avoid deprecation warning in countries_controller_spec
    DEPRECATION WARNING: The success? predicate is deprecated and will be
    removed in Rails 6.0.

--- spec/controllers/countries_controller_spec.rb
+++ spec/controllers/countries_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe CountriesController, type: :controller do
   describe 'GET #index' do
     it 'returns http success' do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to be_successful
     end
 
     it 'loads all of the countries with companies' do
