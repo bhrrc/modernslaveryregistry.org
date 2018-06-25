bd13ab22f45bb7a13b0d95dfa8356d7e59c30164
Chris Lowis <chris.lowis@gmail.com>
2018-06-25 17:05:45 +0100

    Enable pg_trgm extension in database
    Enable the `pg_trgm` extension[1] which allows us to calculate the
    similarity between two strings based on trigram matching. We can use
    this to make the search engine a bit more "fuzzy".
    
    This extension appears to be available in Heroku[2] and RDS[3] so we
    should be safe to enable it now and when we migrate to AWS.
    
    [1] https://www.postgresql.org/docs/9.1/static/pgtrgm.html
    [2]
    https://devcenter.heroku.com/articles/heroku-postgres-extensions-postgis-full-text-search
    [3] https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_21_082254) do
+ActiveRecord::Schema.define(version: 2018_06_25_155843) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
