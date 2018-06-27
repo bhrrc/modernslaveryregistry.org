f7aae7cd1ff3ff8e27ca564fa84420f7d0b971a6
Chris Roos <chris@seagul.co.uk>
2018-06-27 10:37:39 +0100

    Add script to upload snapshots to S3
    I ran this on my laptop and "uploaded" all the snapshots (originals and
    screenshots) to my local storage. It took about 10 minutes and uploaded
    about 4.3GB.

---
+++ script/upload-snapshots-to-s3.rb
@@ -0,0 +1,30 @@
+require 'snapshot'
+
+class Snapshot < ApplicationRecord
+  def original_needs_to_be_uploaded?
+    content_data.present? && !original.attached?
+  end
+  def screenshot_needs_to_be_uploaded?
+    image_content_data.present? && !screenshot.attached?
+  end
+end
+
+Snapshot.find_each(batch_size: 10) do |snapshot|
+  if snapshot.original_needs_to_be_uploaded?
+    puts "Uploading original for snapshot #{snapshot.id}"
+    snapshot.original.attach(
+      io: StringIO.new(snapshot.content_data),
+      filename: 'original',
+      content_type: snapshot.content_type
+    )
+  end
+
+  if snapshot.screenshot_needs_to_be_uploaded?
+    puts "Uploading screenshot for snapshot #{snapshot.id}"
+    snapshot.screenshot.attach(
+      io: StringIO.new(snapshot.image_content_data),
+      filename: 'original',
+      content_type: snapshot.image_content_type
+    )
+  end
+end
