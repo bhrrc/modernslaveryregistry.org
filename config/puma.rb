4e77d23a4c3475c5d2f3cf8f3975fec3d76a8d90
Chris Roos <chris@seagul.co.uk>
2018-07-06 14:46:17 +0100

    Use Rubocop to auto-correct Layout/TrailingWhitespace violation

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,7 @@
 # and maximum; this matches the default thread size of Active Record.
 #
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
-threads 1, 1 
+threads 1, 1
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
