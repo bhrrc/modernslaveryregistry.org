0486df5b14654272c31a19b991276087f7b51adc
Chris Lowis <chris.lowis@gmail.com>
2019-02-15 10:46:53 +0000

    Use call to action data on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   include ApplicationHelper
 
   def index
+    @call_to_action = CallToAction.first
     @statement_stats = StatementStats.new
     @compliance_stats = ComplianceStats.compile
   end
