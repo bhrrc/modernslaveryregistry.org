9ca3948b2f15ebe9b8145e632e939c816fc181fb
Chris Roos <chris@seagul.co.uk>
2019-02-18 12:08:39 +0000

    Add more examples to compliance_stats_spec
    To give us more confidence that we're not breaking the behaviour when
    making changes.
    
    Note that I've excluded example names (/it ... do/) from RuboCop's
    LineLength Cop.

--- spec/models/compliance_stats_spec.rb
+++ spec/models/compliance_stats_spec.rb
@@ -2,6 +2,73 @@ require 'rails_helper'
 
 RSpec.describe ComplianceStats, type: :model do
   describe 'latest_published_statement_ids' do
+    it 'only includes statements associated with legislations that have been marked for inclusion in these compliance stats' do
+      included_legislation = Legislation.create!(include_in_compliance_stats: true,
+                                                 name: 'included-legislation',
+                                                 icon: 'icon')
+      excluded_legislation = Legislation.create!(include_in_compliance_stats: false,
+                                                 name: 'excluded-legislation',
+                                                 icon: 'icon')
+      company1 = Company.create!(name: 'company-1')
+      company2 = Company.create!(name: 'company-2')
+      included_statement = company1.statements.create!(legislations: [included_legislation],
+                                                       published: true,
+                                                       url: 'http://example.com')
+      company2.statements.create!(legislations: [excluded_legislation], published: true, url: 'http://example.com')
+
+      expect(ComplianceStats.latest_published_statement_ids).to contain_exactly(included_statement.id)
+    end
+
+    it 'only includes published statements' do
+      included_legislation = Legislation.create!(include_in_compliance_stats: true,
+                                                 name: 'included-legislation',
+                                                 icon: 'icon')
+      company1 = Company.create!(name: 'company-1')
+      company2 = Company.create!(name: 'company-2')
+      company2.statements.create!(published: false, legislations: [included_legislation], url: 'http://example.com')
+      published_statement = company1.statements.create!(published: true,
+                                                        legislations: [included_legislation],
+                                                        url: 'http://example.com')
+
+      expect(ComplianceStats.latest_published_statement_ids).to contain_exactly(published_statement.id)
+    end
+
+    it 'includes the latest statement for a company by ordering statements by last_year_covered where present' do
+      included_legislation = Legislation.create!(include_in_compliance_stats: true,
+                                                 name: 'included-legislation',
+                                                 icon: 'icon')
+      company1 = Company.create!(name: 'company-1')
+      company1.statements.create!(last_year_covered: 2018,
+                                  published: true,
+                                  legislations: [included_legislation],
+                                  url: 'http://example.com')
+      latest_statement = company1.statements.create!(last_year_covered: 2019,
+                                                     published: true,
+                                                     legislations: [included_legislation],
+                                                     url: 'http://example.com')
+
+      expect(ComplianceStats.latest_published_statement_ids).to contain_exactly(latest_statement.id)
+    end
+
+    it 'includes the latest statement for a company by additionally ordering statements by date_seen to differentiate statements with the same last_year_covered value' do
+      included_legislation = Legislation.create!(include_in_compliance_stats: true,
+                                                 name: 'included-legislation',
+                                                 icon: 'icon')
+      company1 = Company.create!(name: 'company-1')
+      company1.statements.create!(date_seen: Date.parse('2019-01-01'),
+                                  last_year_covered: 2019,
+                                  published: true,
+                                  legislations: [included_legislation],
+                                  url: 'http://example.com')
+      recently_seen_statement = company1.statements.create!(date_seen: Date.parse('2019-02-01'),
+                                                            last_year_covered: 2019,
+                                                            published: true,
+                                                            legislations: [included_legislation],
+                                                            url: 'http://example.com')
+
+      expect(ComplianceStats.latest_published_statement_ids).to contain_exactly(recently_seen_statement.id)
+    end
+
     it 'returns a single statement even when that statement has multiple companies' do
       company1 = Company.create!(name: 'company-1')
       company2 = Company.create!(name: 'company-2')
