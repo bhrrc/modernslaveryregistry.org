47c0045b85b30ba73d50918c15fa2d6810cbe621
Chris Roos <chris@seagul.co.uk>
2019-02-18 16:28:16 +0000

    Add specs for Company#latest_statement
    In preparation for updating this method so that it considers statements
    associated with this company that have been produced/published by
    another company.

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -39,4 +39,24 @@ RSpec.describe Company, type: :model do
 
     expect(company.statements).to eq([latest_statement, earliest_statement])
   end
+
+  it 'uses last_year_covered to determine the latest statement' do
+    company = Company.create!(name: 'company-name')
+    company.statements.create!(last_year_covered: 2018, url: 'http://older-statement')
+    newer_statement = company.statements.create!(last_year_covered: 2019, url: 'http://newer-statement')
+
+    expect(company.latest_statement).to eq(newer_statement)
+  end
+
+  it 'uses date_seen to determine the latest statement when last_year_covered is identical' do
+    company = Company.create!(name: 'company-name')
+    company.statements.create!(date_seen: Date.parse('2019-01-01'),
+                               last_year_covered: 2019,
+                               url: 'http://seen-less-recently')
+    newer_statement = company.statements.create!(date_seen: Date.parse('2019-02-01'),
+                                                 last_year_covered: 2019,
+                                                 url: 'http://seen-more-recently')
+
+    expect(company.latest_statement).to eq(newer_statement)
+  end
 end
