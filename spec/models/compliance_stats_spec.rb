7af2dfa9773c91b31eb419e297c450403618963a
Chris Lowis <chris.lowis@gmail.com>
2019-02-18 15:44:58 +0000

    Add some tests around the individual stat calculations
    I want to add calculation by industry sector and this will give me
    confidence that I haven't broken anything.

--- spec/models/compliance_stats_spec.rb
+++ spec/models/compliance_stats_spec.rb
@@ -3,6 +3,73 @@ require 'rails_helper'
 RSpec.describe ComplianceStats, type: :model do
   let(:subject) { ComplianceStats.new }
 
+  describe 'statistics' do
+    let(:legislation) do
+      Legislation.create!(include_in_compliance_stats: true,
+                          name: 'included-legislation',
+                          icon: 'icon')
+    end
+
+    let(:company1) { Company.create!(name: 'company-1') }
+    let(:company2) { Company.create!(name: 'company-2') }
+
+    let!(:fully_compliant_statement) do
+      company1.statements.create!(legislations: [legislation],
+                                  published: true,
+                                  url: 'http://example.com',
+                                  approved_by_board: 'Yes',
+                                  link_on_front_page: true,
+                                  signed_by_director: true
+                                 )
+    end
+
+    let!(:fully_incompliant_statement) do
+      company2.statements.create!(legislations: [legislation],
+                                  published: true,
+                                  url: 'http://example.com',
+                                  approved_by_board: 'No',
+                                  link_on_front_page: false,
+                                  signed_by_director: false
+                                 )
+    end
+
+    it 'includes both statements in the total' do
+      expect(subject.total).to eq(2)
+    end
+
+    it 'includes one statement in the approved_by_board_count' do
+      expect(subject.approved_by_board_count).to eq(1)
+    end
+
+    it 'includes one statement in the link_on_front_page_count' do
+      expect(subject.link_on_front_page_count).to eq(1)
+    end
+
+    it 'includes one statement in the signed_by_director_count' do
+      expect(subject.signed_by_director_count).to eq(1)
+    end
+
+    it 'includes one statement in the fully_compliant_count' do
+      expect(subject.fully_compliant_count).to eq(1)
+    end
+
+    it 'calculates the percent_approved_by_board correctly' do
+      expect(subject.percent_approved_by_board).to eq(50)
+    end
+
+    it 'calculates the percent_link_on_front_page' do
+      expect(subject.percent_link_on_front_page).to eq(50)
+    end
+
+    it 'calculates the percent_signed_by_director' do
+      expect(subject.percent_signed_by_director).to eq(50)
+    end
+
+    it 'calculates the percent_fully_compliant' do
+      expect(subject.percent_fully_compliant).to eq(50)
+    end
+  end
+
   describe 'latest_published_statement_ids' do
     it 'only includes statements associated with legislations that have been marked for inclusion in these compliance stats' do
       included_legislation = Legislation.create!(include_in_compliance_stats: true,
