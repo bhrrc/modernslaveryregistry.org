a1cbc1e81fca4d0a3e123f3107cba66f9af07b2e
Chris Roos <chris@seagul.co.uk>
2019-02-18 16:28:16 +0000

    Add specs for Company#latest_published_statement
    In preparation for updating this method so that it considers statements
    associated with this company that have been produced/published by
    another company.

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -59,4 +59,38 @@ RSpec.describe Company, type: :model do
 
     expect(company.latest_statement).to eq(newer_statement)
   end
+
+  it 'uses last_year_covered to determine the latest published statement' do
+    company = Company.create!(name: 'company-name')
+    _newest_but_unpublished_statement = company.statements.create!(published: false,
+                                                                   last_year_covered: 2019,
+                                                                   url: 'http://example.com')
+    _older_published_statement = company.statements.create!(published: true,
+                                                            last_year_covered: 2017,
+                                                            url: 'http://example.com')
+    newer_published_statement = company.statements.create!(published: true,
+                                                           last_year_covered: 2018,
+                                                           url: 'http://example.com')
+
+    expect(company.latest_published_statement).to eq(newer_published_statement)
+  end
+
+  it 'uses date_seen to determine the latest published statement when last_year_covered is identical' do
+    last_year_covered = 2018
+    company = Company.create!(name: 'company-name')
+    _most_recently_seen_but_unpublished_state = company.statements.create!(published: false,
+                                                                           date_seen: Date.parse('2019-02-01'),
+                                                                           last_year_covered: last_year_covered,
+                                                                           url: 'http://example.com')
+    _less_recently_seen_published_statement = company.statements.create!(published: true,
+                                                                         date_seen: Date.parse('2019-01-01'),
+                                                                         last_year_covered: last_year_covered,
+                                                                         url: 'http://example.com')
+    more_recently_seen_published_statement = company.statements.create!(published: true,
+                                                                        date_seen: Date.parse('2019-01-02'),
+                                                                        last_year_covered: last_year_covered,
+                                                                        url: 'http://example.com')
+
+    expect(company.latest_published_statement).to eq(more_recently_seen_published_statement)
+  end
 end
