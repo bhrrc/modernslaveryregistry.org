d7f1610eef8d61b715068eb591848f1c87ce9a9a
Chris Lowis <chris.lowis@gmail.com>
2019-02-19 11:46:28 +0000

    Allow the compliance stats to be filtered by industry
    This commit filters the data used to display the compliance stats on
    the home page by industry based on the id passed in the `?industry=`
    URL parameter. Allowing the user to select the parameter using a UI
    will come in a subsequent commit.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,16 @@ class WelcomeController < ApplicationController
   def index
     @call_to_actions = CallToAction.all
     @statement_stats = StatementStats.new
-    @compliance_stats = ComplianceStats.new
+    @compliance_stats = ComplianceStats.new(industry: industry_filter)
+  end
+
+  private
+
+  def industry_filter
+    return false unless params[:industry]
+
+    Industry.find(params[:industry].to_i)
+  rescue ActiveRecord::RecordNotFound
+    false
   end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -22,5 +22,29 @@ RSpec.describe WelcomeController, type: :controller do
       get :index
       expect(assigns(:compliance_stats)).to eq(:compliance_stats)
     end
+
+    describe 'providing an industry in the URL parameters' do
+      let(:industry) { Industry.create!(name: 'Software') }
+
+      context 'when the industry id provided is valid' do
+        it 'assigns compliance stats filtered by industry to the view' do
+          expect(ComplianceStats).to receive(:new).with(industry: industry).and_return(:filtered_compliance_stats)
+
+          get :index, params: { industry: industry.id }
+
+          expect(assigns(:compliance_stats)).to eq(:filtered_compliance_stats)
+        end
+      end
+
+      context 'when the industry id provided is invalid' do
+        it 'assigns unfiltered compliance stats to the view' do
+          expect(ComplianceStats).to receive(:new).with(industry: false).and_return(:unfiltered_compliance_stats)
+
+          get :index, params: { industry: 'foo' }
+
+          expect(assigns(:compliance_stats)).to eq(:unfiltered_compliance_stats)
+        end
+      end
+    end
   end
 end
