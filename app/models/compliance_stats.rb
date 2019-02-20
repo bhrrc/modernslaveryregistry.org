1babce4baccad250a2966a374b5bf6418f3bfbc5
Chris Roos <chris@seagul.co.uk>
2019-02-20 13:36:13 +0000

    Improve performance of ComplianceStats#approved_by_board_count
    We think the use of `in` with a large array (in the region of 6000
    statement IDs) might be causing the problems we're seeing with the site
    on AWS.
    
    This change certainly seems to perform better based on some manual
    testing in the Rails console.
    
    I've introduced quite a lot of duplication in this commit but hope to
    DRY it up in future.

--- app/models/compliance_stats.rb
+++ app/models/compliance_stats.rb
@@ -15,7 +15,11 @@ class ComplianceStats
   end
 
   def approved_by_board_count
-    statements.where(approved_by_board: 'Yes').count
+    if @industry
+      latest_published_statements_approved_by_board_count_for(@industry)
+    else
+      latest_published_statements_approved_by_board_count
+    end
   end
 
   def link_on_front_page_count
@@ -93,6 +97,58 @@ class ComplianceStats
       INNER JOIN companies ON published_statements.company_id = companies.id
       WHERE reverse_publication_order = 1
       AND companies.industry_id = #{industry.id}
+    SQL
+
+    Statement.connection.select_value(sql)
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  # rubocop:disable Metrics/MethodLength
+  def latest_published_statements_approved_by_board_count
+    sql = <<~SQL
+      WITH statements_included_in_compliance_stats AS (
+        SELECT statements.* FROM statements
+        INNER JOIN legislation_statements ON statements.id = legislation_statements.statement_id
+        INNER JOIN legislations ON legislations.id = legislation_statements.legislation_id
+        WHERE legislations.include_in_compliance_stats IS TRUE
+      ),
+      published_statements AS (
+        SELECT statements.*,
+               ROW_NUMBER() OVER(PARTITION BY statements.company_id
+                                 ORDER BY statements.last_year_covered DESC, statements.date_seen DESC) AS reverse_publication_order
+        FROM statements_included_in_compliance_stats AS statements
+        WHERE published IS TRUE )
+
+      SELECT COUNT(id) FROM published_statements
+      WHERE reverse_publication_order = 1
+      AND approved_by_board = 'Yes'
+    SQL
+
+    Statement.connection.select_value(sql)
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  # rubocop:disable Metrics/MethodLength
+  def latest_published_statements_approved_by_board_count_for(industry)
+    sql = <<~SQL
+      WITH statements_included_in_compliance_stats AS (
+        SELECT statements.* FROM statements
+        INNER JOIN legislation_statements ON statements.id = legislation_statements.statement_id
+        INNER JOIN legislations ON legislations.id = legislation_statements.legislation_id
+        WHERE legislations.include_in_compliance_stats IS TRUE
+      ),
+      published_statements AS (
+        SELECT statements.*,
+               ROW_NUMBER() OVER(PARTITION BY statements.company_id
+                                 ORDER BY statements.last_year_covered DESC, statements.date_seen DESC) AS reverse_publication_order
+        FROM statements_included_in_compliance_stats AS statements
+        WHERE published IS TRUE )
+
+      SELECT COUNT(published_statements.id) FROM published_statements
+      INNER JOIN companies ON published_statements.company_id = companies.id
+      WHERE reverse_publication_order = 1
+      AND companies.industry_id = #{industry.id}
+      AND approved_by_board = 'Yes'
     SQL
 
     Statement.connection.select_value(sql)
