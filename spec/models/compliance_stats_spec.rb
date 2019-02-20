4a1a1e2cfa0ecb38004c3b825b8a7747baa7c9f9
Chris Roos <chris@seagul.co.uk>
2019-02-20 14:51:49 +0000

    Add ComplianceStats examples about ordering statements
    I want to improve the test coverage of this class to give me confidence
    to make changes to the implementation.

--- spec/models/compliance_stats_spec.rb
+++ spec/models/compliance_stats_spec.rb
@@ -105,6 +105,134 @@ RSpec.describe ComplianceStats, type: :model do
     end
   end
 
+  context 'when there are multiple published statements covering different years' do
+    let(:legislation) do
+      Legislation.create!(include_in_compliance_stats: true,
+                          name: 'included-legislation',
+                          icon: 'icon')
+    end
+
+    let!(:newer_statement) do
+      company1.statements.create!(legislations: [legislation],
+                                  published: true,
+                                  url: 'http://example.com',
+                                  approved_by_board: 'Yes',
+                                  link_on_front_page: true,
+                                  signed_by_director: true,
+                                  last_year_covered: 2019)
+    end
+
+    let!(:older_statement) do
+      company1.statements.create!(legislations: [legislation],
+                                  published: true,
+                                  url: 'http://example.com',
+                                  approved_by_board: 'No',
+                                  link_on_front_page: false,
+                                  signed_by_director: false,
+                                  last_year_covered: 2018)
+    end
+
+    it 'includes a single statement in the total' do
+      expect(subject.total).to eq(1)
+    end
+
+    it 'includes the newer statement in the approved_by_board_count' do
+      expect(subject.approved_by_board_count).to eq(1)
+    end
+
+    it 'includes the newer statement in the link_on_front_page_count' do
+      expect(subject.link_on_front_page_count).to eq(1)
+    end
+
+    it 'includes the newer statement in the signed_by_director_count' do
+      expect(subject.signed_by_director_count).to eq(1)
+    end
+
+    it 'includes the newer statement in the fully_compliant_count' do
+      expect(subject.fully_compliant_count).to eq(1)
+    end
+
+    it 'calculates the percent_approved_by_board correctly' do
+      expect(subject.percent_approved_by_board).to eq(100)
+    end
+
+    it 'calculates the percent_link_on_front_page' do
+      expect(subject.percent_link_on_front_page).to eq(100)
+    end
+
+    it 'calculates the percent_signed_by_director' do
+      expect(subject.percent_signed_by_director).to eq(100)
+    end
+
+    it 'calculates the percent_fully_compliant' do
+      expect(subject.percent_fully_compliant).to eq(100)
+    end
+  end
+
+  context 'when there are multiple published statements that were first seen at different times' do
+    let(:legislation) do
+      Legislation.create!(include_in_compliance_stats: true,
+                          name: 'included-legislation',
+                          icon: 'icon')
+    end
+
+    let!(:more_recently_seen_statement) do
+      company1.statements.create!(legislations: [legislation],
+                                  published: true,
+                                  url: 'http://example.com',
+                                  approved_by_board: 'Yes',
+                                  link_on_front_page: true,
+                                  signed_by_director: true,
+                                  date_seen: Date.parse('1 Feb 2019'))
+    end
+
+    let!(:less_recently_seen_statement) do
+      company1.statements.create!(legislations: [legislation],
+                                  published: true,
+                                  url: 'http://example.com',
+                                  approved_by_board: 'No',
+                                  link_on_front_page: false,
+                                  signed_by_director: false,
+                                  date_seen: Date.parse('1 Jan 2019'))
+    end
+
+    it 'includes a single statement in the total' do
+      expect(subject.total).to eq(1)
+    end
+
+    it 'includes the more recently seen statement in the approved_by_board_count' do
+      expect(subject.approved_by_board_count).to eq(1)
+    end
+
+    it 'includes the more recently seen statement in the link_on_front_page_count' do
+      expect(subject.link_on_front_page_count).to eq(1)
+    end
+
+    it 'includes the more recently seen statement in the signed_by_director_count' do
+      expect(subject.signed_by_director_count).to eq(1)
+    end
+
+    it 'includes the more recently seen statement in the fully_compliant_count' do
+      expect(subject.fully_compliant_count).to eq(1)
+    end
+
+    it 'calculates the percent_approved_by_board correctly' do
+      expect(subject.percent_approved_by_board).to eq(100)
+    end
+
+    it 'calculates the percent_link_on_front_page' do
+      expect(subject.percent_link_on_front_page).to eq(100)
+    end
+
+    it 'calculates the percent_signed_by_director' do
+      expect(subject.percent_signed_by_director).to eq(100)
+    end
+
+    it 'calculates the percent_fully_compliant' do
+      expect(subject.percent_fully_compliant).to eq(100)
+    end
+  end
+
   describe '#statements' do
     let(:software) { Industry.create! name: 'Software' }
     let(:software_company) { Company.create!(name: 'company-1', industry: software) }
