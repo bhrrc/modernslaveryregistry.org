885bad564cc162a7029a01c4221a0fbe7ac18511
Chris Roos <chris@seagul.co.uk>
2019-02-26 15:35:02 +0000

    Display additional companies covered by the statement in admin

--- spec/views/admin/statements/_details.html.erb_spec.rb
+++ spec/views/admin/statements/_details.html.erb_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe 'admin/statements/_details.html.erb', type: :view do
     url = admin_company_path(company)
     expect(rendered).to have_css("div[data-content='company_name'] a[href='#{url}']", text: 'company-name')
   end
+
+  it 'links to additional companies covered by this statement' do
+    another_company = stub_model(Company, name: 'another-company')
+    statement.additional_companies_covered << another_company
+
+    render partial: 'details', locals: { statement: statement }
+
+    url = admin_company_path(another_company)
+    expect(rendered).to have_css("div[data-content='additional_companies'] a[href='#{url}']", text: 'another-company')
+  end
 end
