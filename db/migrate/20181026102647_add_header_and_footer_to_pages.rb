64a0ec0f2b6891800844a807a252f5dc653584dc
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2019-03-17 23:08:51 +0000

    update_all rather than update(:all)

--- db/migrate/20181026102647_add_header_and_footer_to_pages.rb
+++ db/migrate/20181026102647_add_header_and_footer_to_pages.rb
@@ -3,6 +3,6 @@ class AddHeaderAndFooterToPages < ActiveRecord::Migration[5.2]
     add_column :pages, :header, :boolean, default: true
     add_column :pages, :footer, :boolean, default: true
 
-    Page.update(:all, header: true, footer: true);
+    Page.update_all(header: true, footer: true);
   end
 end
