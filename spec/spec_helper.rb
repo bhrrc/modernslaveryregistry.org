64a522fa7b975b9d4e9a2fe24080dad430edb08e
Aleksey Zolotov <myxrome@gmail.com>
2020-03-02 15:10:23 +0400

    added vcr exception for es http calls.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -100,6 +100,9 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'fixtures/vcr_cassettes'
   config.hook_into :webmock
+  config.ignore_request do |request|
+    URI(request.uri).port == 9200
+  end
 end
 
 class ScreenGrab
