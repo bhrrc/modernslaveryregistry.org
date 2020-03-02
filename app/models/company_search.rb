59d575a57a04978191944b7d6d2edfdc69269078
Aleksey Zolotov <myxrome@gmail.com>
2020-03-02 15:06:34 +0400

    rewrote company search functionality.

--- app/models/company_search.rb
+++ app/models/company_search.rb
@@ -1,62 +1,24 @@
 class CompanySearch
   def initialize(company_name: nil, industries: [], countries: [], legislations: [])
     @company_name = company_name
-    @industries = industries
-    @countries = countries
-    @legislations = legislations
+
+    @where = {}
+    @where.merge!({ industry_id: industries }) if industries&.any?
+    @where.merge!({ country_id: countries }) if countries&.any?
+    @where.merge!({ legislation_ids: legislations }) if legislations&.any?
   end
 
   def results
-    company_scope
-      .merge(filter_by_countries)
-      .merge(filter_by_industries)
-      .merge(filter_by_legislations)
-      .distinct
-  end
-
-  def industry_stats
-    IndustryGroupCounts.new(results).stats
+    Company.search(
+      @company_name.presence || '*',
+      fields: [:name, :related_companies],
+      where: @where
+    )
   end
 
   def statement_count_for(company)
     return company.published_statements.count if @legislations.blank?
 
     company.published_statements.joins(:legislations).where(legislations: { id: @legislations }).count
-  end
-
-  private
-
-  def company_scope
-    if @company_name&.present?
-      company_name_query = @company_name.split.join(' & ')
-      Company.where(company_name_query_sql, company_name_query)
-    else
-      Company.all.order(:name)
-    end
-  end
-
-  def company_name_query_sql
-    <<-SQL
-      to_tsvector(concat_ws(' ', companies.name, companies.related_companies)) @@
-      ts_rewrite(plainto_tsquery(?), 'SELECT target, substitute FROM search_aliases')
-    SQL
-  end
-
-  def filter_by_countries
-    return Company.all if @countries.blank?
-
-    Company.joins(:country).where(companies: { country: @countries })
-  end
-
-  def filter_by_industries
-    return Company.all if @industries.blank?
-
-    Company.joins(:industry).where(companies: { industry: @industries })
-  end
-
-  def filter_by_legislations
-    return Company.all if @legislations.blank?
-
-    Company.joins(statements: :legislations).where(statements: { legislations: { id: @legislations } })
   end
 end
