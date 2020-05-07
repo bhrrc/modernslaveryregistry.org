a4d2324809d7f5d7ade57be966e13964a791b790
Matthew Ford <matt@bitzesty.com>
2020-05-07 16:41:47 +0100

    tweak throttle limits

--- app/workers/content_extraction_worker.rb
+++ app/workers/content_extraction_worker.rb
@@ -6,7 +6,7 @@ class ContentExtractionWorker
 
   sidekiq_throttle({
     # Allow maximum 10 concurrent jobs of this class at a time.
-    :concurrency => { :limit => 10 },
+    :concurrency => { :limit => 16 },
     :ttl => 20.minutes.to_i
   })
 
