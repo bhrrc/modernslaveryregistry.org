e64fc862eee4880513e9006b32b16d3f2694b6c9
Matthew Ford <matt@bitzesty.com>
2020-05-07 14:45:11 +0100

    Typo

--- app/workers/content_extraction_worker.rb
+++ app/workers/content_extraction_worker.rb
@@ -3,7 +3,7 @@ class ContentExtractionWorker
   sidekiq_options retry: 0
 
   def perform(statement_id)
-    statement = Statement.includes(snapshot).find(statement_id)
+    statement = Statement.includes(:snapshot).find(statement_id)
     statement.extract_content_from_statement
   end
 end
