7383067b11ea0e48587ff5ef25c001bedeca290e
Matthew Ford <matt@bitzesty.com>
2020-06-18 12:20:46 +0100

    Add searchkick config

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -1,1 +1,7 @@
 Searchkick.timeout = 900
+Searchkick.redis = ConnectionPool.new { Redis.new }
+Searchkick.client_options = {
+  retry_on_failure: true,
+  transport_options:
+    { request: { timeout: 250 } }
+}
