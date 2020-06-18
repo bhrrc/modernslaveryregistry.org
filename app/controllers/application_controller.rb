9929da2258051e538011bdf7cf09c820033a3b8a
Matthew Ford <matt@bitzesty.com>
2020-06-18 12:30:43 +0100

    Optimising the java library for content extraction (#97)
    Update Henkei with the auto extraction mode
    Speed up deployments by using corretto jvm
    Disable logging for health check route and add sampling
    Extend url_expires_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   layout :layout_by_resource
 
+  before_action :sample_requests_for_scout
+
   def after_sign_in_path_for(user)
     stored_location_for(user) || default_after_sign_in_path_for(user)
   end
@@ -22,4 +24,18 @@ class ApplicationController < ActionController::Base
       'application'
     end
   end
+
+  def sample_requests_for_scout
+    # Sample rate should range from 0-1:
+    # * 0: captures no requests
+    # * 0.70: captures 70% of requests
+    # * 1: captures all requests
+    sample_rate = 0.70
+  
+    if rand > sample_rate
+      Rails.logger.debug("[Scout] Ignoring request: #{request.original_url}")
+      ScoutApm::Transaction.ignore!
+    end
+  end
+  
 end
---
+++ config/initializers/active_storage.rb
@@ -0,0 +1,1 @@
+ActiveStorage::Service.url_expires_in = 24.hours
